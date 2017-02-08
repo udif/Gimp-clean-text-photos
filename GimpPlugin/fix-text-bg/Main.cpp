445852bec133f41b37b333ba234c46a8a283786c
Udi Finkelstein <github@udifink.com>
2017-02-08 20:07:32 +0200

    First version that works reasonably well! Remaining problems are when there are large blobs of ink, which are mistakenly detected as background.

--- GimpPlugin/fix-text-bg/Main.cpp
+++ GimpPlugin/fix-text-bg/Main.cpp
@@ -18,7 +18,7 @@ const GimpPlugInInfo	PLUG_IN_INFO =
 {
 	NULL, NULL, Query, Run
 };
-PCSTR PLUGIN_NAME = "plug-in-Invert";
+PCSTR PLUGIN_NAME = "plug-in-Fix-Text-Bg";
 
 void	Msg(PCSTR psFmt, ...)
 {
@@ -102,12 +102,12 @@ void	Query()
 	(*pfnGimpInstallProcedure)
 	(
 		PLUGIN_NAME,
-		"Invert Color",
-		"Invert color.",
-		"Um6ra1",
-		"Copyright (C) Um6ra1",
-		"2016",
-		"_Invert",
+		"Fix-Text-Bg",
+		"Clear text background of text pages taken by a camera. Adapts to varying background levels and shadows across the whole image",
+		"udif",
+		"Copyright (C) udif, Um6ra1",
+		"2017",
+		"_FixTextBg",
 		"RGB*, GRAY*",
 		GIMP_PLUGIN,
 		G_N_ELEMENTS(args), 0,
@@ -163,9 +163,9 @@ void	ImgProc(Dll &libGimp, GimpDrawable *pDrawable)
 	PFNFUNC	pfnGimpTileWidth = libGimp.GetFunction("gimp_tile_width");
 
 	(*pfnGimpMaskBounds)(pDrawable->drawable_id, &x1, &y1, &x2, &y2);
-	int channels = (int)(*pfnGimpDrawableBpp)(pDrawable->drawable_id);
-	int width = x2 - x1;
-	int height = y2 - y1;
+	const int channels = (int)(*pfnGimpDrawableBpp)(pDrawable->drawable_id);
+	const int width = x2 - x1;
+	const int height = y2 - y1;
 	//::Msg("x, y, w, h = %d, %d, %d, %d", x1, y1, width, height);
 	//(*pfnGimpTileCacheNTiles)(CEILDIV(pDrawable->width, (int)(*pfnGimpTileWidth)()));
 	(*pfnGimpPixelRgnInit)(&rgnDst, pDrawable, x1, y1, width, height, true, true);
@@ -174,18 +174,127 @@ void	ImgProc(Dll &libGimp, GimpDrawable *pDrawable)
 	std::vector<BYTE>	buf(width * height * channels);
 
 	(*pfnGimpPixelRgnGetRect)(&rgnSrc, &buf[0], x1, y1, width, height);
-
+	const int kernel_size = 50;
+	const int inner_size = 20;
+	const int inner_step = 2 * inner_size + 1;
+	const int kernel_area = (kernel_size * 2 + 1) * (kernel_size * 2 + 1);
+	// Desaturate first, using "Luminocity" algorithm
+	// store it in the red channel only, because we'll modify it anyhow
 	REP(y, height)
 	{
 		REP(x, width)
 		{
 			UINT32 &pixel = *(UINT32 *)&buf[channels * (width * y + x)];
-			REP(c, channels)
-			{
-				if (c >= 3)	break;
-				BYTE	&p = ((BYTE *)&pixel)[c];
-				p = ~p;
+			BYTE	&r = ((BYTE *)&pixel)[0];
+			BYTE	&g = ((BYTE *)&pixel)[1];
+			BYTE	&b = ((BYTE *)&pixel)[2];
+			r = (21 * r + 72 * g + 7 * b) / 100;
+			//g = r;
+			//b = r;
+		}
+	}
+	// Now, do your work
+	typedef struct {
+		int val;
+		int idx;
+	} histidx;
+	histidx hist[256];
+#ifdef _DEBUG
+	// This is enabled automatically when running a Visual C++ debug build.
+	// You can use this
+	::Msg("If you would like to debug, now is a good time to select 'Debug/Attach to process'\n"
+		"From the Visual Studio menu (<Ctrl>+<Alt>+P also works).\n");
+#endif
+	for (int y = kernel_size; y < height - kernel_size; y += inner_step) {
+		for (int x = kernel_size; x < width - kernel_size; x += inner_step) {
+			memset(hist, 0, sizeof(hist));
+			int avg_val = 0;
+			for (int dy = -kernel_size; dy <= kernel_size; dy++) {
+				for (int dx = -kernel_size; dx <= kernel_size; dx++) {
+					UINT32 &pixel = *(UINT32 *)&buf[channels * (width * (y + dy) + x + dx)];
+					BYTE &val = ((BYTE *)&pixel)[0];
+					hist[val].val++;
+					avg_val += val;
+				}
 			}
+			avg_val /= kernel_area;
+			// Find histogram maximum point
+			int max_val = hist[0].val;
+			int max_idx = 0;
+			for (int i = 1; i < 256; i++) {
+				if (hist[i].val > max_val) {
+					max_val = hist[i].val;
+					max_idx = i;
+				}
+			}
+			// Find first knee point in histogram below max_val
+			int last_val = max_val;
+			int bg_thresh;
+			for (bg_thresh = max_idx-1; bg_thresh >= 0; bg_thresh--) {
+				if (last_val < hist[bg_thresh].val)
+					break;
+				last_val = hist[bg_thresh].val;
+			}
+#if 0
+			for (int i = 0; i < 256; i++) {
+				hist[i].idx = i;
+			}
+			std::qsort(hist, 256, 8, [](const void* a, const void* b)
+			{
+				int arg1 = static_cast<const histidx*>(a)->val;
+				int arg2 = static_cast<const histidx*>(b)->val;
+
+				if (arg1 < arg2) return -1;
+				if (arg1 > arg2) return 1;
+				return 0;
+			});
+			// hist[255] is peak, we assume this is background level
+			// Find lowest valley above background level, and choose as white threshold
+			int bg_thresh=256;
+			int bg_val = hist[0].val;
+			int i;
+			for (i = 0; i < 255; i++) {
+				if ((bg_val < hist[i].val) && (bg_thresh < 256))
+					break;
+				if ((hist[i].idx >= hist[255].idx) && (hist[i].idx < bg_thresh)) {
+					bg_thresh = hist[i].idx;
+					bg_val = hist[i].val;
+				}
+			}
+			static bool once = false;
+			if (!once) {
+				::Msg(s);
+				once = true;
+			}
+#endif
+			// If avg_val is higher than peak it means that the peak is the darker characters
+			// Increase threshold to prevent earsing characters
+			if (max_idx < avg_val)
+				bg_thresh = avg_val;
+			// Write result in g,b
+			bg_thresh -= 10;
+			for (int dy = -inner_size; dy <= inner_size; dy++) {
+				for (int dx = -inner_size; dx <= inner_size; dx++) {
+					UINT32 &pixel = *(UINT32 *)&buf[channels * (width * (y + dy) + x + dx)];
+					BYTE	&r = ((BYTE *)&pixel)[0];
+					BYTE	&g = ((BYTE *)&pixel)[1];
+					BYTE	&b = ((BYTE *)&pixel)[2];
+					if (r > bg_thresh) {
+						g = b = 255;
+					}
+				}
+			}
+		}
+	}
+	// Now copy to r as well
+	REP(y, height)
+	{
+		REP(x, width)
+		{
+			UINT32 &pixel = *(UINT32 *)&buf[channels * (width * y + x)];
+			BYTE	&r = ((BYTE *)&pixel)[0];
+			BYTE	&g = ((BYTE *)&pixel)[1];
+			r = g;
 		}
 	}
 
