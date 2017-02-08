efd72551b22f29e11abaa21c0ba6a407e4099a2f
Udi Finkelstein <github@udifink.com>
2017-02-08 20:03:04 +0200

    Tailor makefile for Visual C++ and buold both debug and release versions.

--- GimpPlugin/fix-text-bg/Makefile.mk
+++ GimpPlugin/fix-text-bg/Makefile.mk
@@ -1,22 +1,34 @@
 # Makefile
-TGT	= Invert.exe
-
+TGT	= fix-text-bg.exe
+TGTD = fix-text-bg-dbg.exe
 OBJS = Main.obj 
+CC=cl
 
-CFLAGS =# /LD
+CFLAGS = # /LD
+DEBUG = /Zi
+RELEASE = /Ox
 #INCLUDE =
 #LIBS	=
 #LIBS	+= -ladvapi32 -luser32 -lgdi32 -lkernel32 -lmingwex
 
 .PHONY: all
-all: $(TGT)
+all: $(TGT) $(TGTD)
 
-$(TGT): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) $(LIBS) -o $@
+$(TGT): Main.cpp
+	$(CC) -c /FoRelease\Main.obj $(RELEASE) $(CFLAGS) $(LDFLAGS) $(INCLUDE) Main.cpp
+	$(CC) $(CFLAGS) /Fe$@ Release\Main.obj $(LIBS)
 	@echo Done.
 
-.cpp.obj:
-	$(CC) -c $(CFLAGS) $(LDFLAGS) $(INCLUDE) $<
+$(TGTD): Main.cpp
+	$(CC) -c /FoDebug\Main.obj $(DEBUG) $(CFLAGS) $(LDFLAGS) $(INCLUDE) Main.cpp
+	$(CC) $(CFLAGS) /Fe$@ Debug\Main.obj $(LIBS)
+	@echo Done.
+
+#$(TGT): $(OBJS)
+#	$(CC) $(CFLAGS) $(OBJS) $(LIBS) -o $@
+#
+#.cpp.obj:
+#	$(CC) -c $(DEBUG) $(CFLAGS) $(LDFLAGS) $(INCLUDE) $<
 
 .PHONY: clean
 clean:
